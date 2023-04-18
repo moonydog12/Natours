dbde6290856e7abcddfae37d9238650d3438c8a0
moonydog12 <losididog@gmail.com>
2023-04-18 22:39:57 +0800

    整理檔案架構

---
+++ routes/tourRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const {
+  getAllTours,
+  createTour,
+  getTour,
+  deleteTour,
+  updateTour,
+} = require('../controllers/tourController');
+
+const router = express.Router();
+
+router.route('/').get(getAllTours).post(createTour);
+router.route('/:id').get(getTour).delete(deleteTour).patch(updateTour);
+
+module.exports = router;
