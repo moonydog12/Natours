7eb2c4ceb7866a8b277aeaec92612266445b4e68
moonydog12 <losididog@gmail.com>
2023-04-25 20:23:34 +0800

    實作條件篩選/進階條件篩選

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,7 +3,22 @@ const Tour = require('../models/tourModel');
 // Handlers
 const getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find({});
+    // Build query
+
+    // 1)條件篩選
+    const queryObj = { ...req.query }; // 拷貝一個新的 request 物件
+    const excludeFields = ['page', 'sort', 'limit', 'fields'];
+    excludeFields.forEach((el) => delete queryObj[el]);
+
+    // 2)進階篩選
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`); // 為特定 mongoose 運算子新增字符
+    const query = Tour.find(JSON.parse(queryStr));
+
+    // 執行 query
+    const tours = await query;
+
+    // 回傳結果
     res.status(200).json({
       status: 'successful',
       results: tours.length,
