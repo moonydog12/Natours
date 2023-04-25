de59272e6171b54d2bba86f9681cc1e05adb68fd
moonydog12 <losididog@gmail.com>
2023-04-25 17:11:58 +0800

    新增完整 model schema

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: '../../config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection successfully');
+  });
+
+// Read json file
+const tours = JSON.parse(fs.readFileSync('./tours-simple.json', 'utf-8'));
+
+// import data into DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+// delete all data from db
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data deleted');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
