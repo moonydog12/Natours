e59584ab91d1910c2be89eac963cb3306909a92f
moonydog12 <losididog@gmail.com>
2023-06-09 16:40:30 +0800

    新增 Tour page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -14,10 +14,19 @@ const getOverview = catchAsync(async (req, res) => {
   });
 });
 
-const getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'Test',
+const getTour = catchAsync(async (req, res) => {
+  // get the data for the requested tour(including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'reviews rating user',
   });
-};
+
+  // Build template
+
+  // Render template using data
+  res.status(200).render('tour', {
+    tour,
+  });
+});
 
 module.exports = { getOverview, getTour };
